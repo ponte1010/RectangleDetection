d0f0b72fec881e02b9751de77fdc1dc8491df0d8
ponte1010 <t.miwa.528@nitech.jp>
2020-12-26 14:45:40 +0900

    Initial Commit

---
+++ RectangleDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RectangleDetection
+//
+//  Created by miwa on 2020/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
